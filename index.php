1e40daa7af9eaf46aa71cd16718e0ef8040a6ab6
Yeahboys04 <ryankorban2004@gmail.com>
2024-11-06 15:51:41 +0100

    info dans index.php

--- index.php
+++ index.php
@@ -8,3 +8,6 @@ echo "Bonjour je passe l'ASRR CIAO";
 // dans config nrv.db.ini il faut que ça soit sous cette forme pour que ça marche avec mon serveur local
 // wsl donc soyez pas choquer en voyant dsn = "mysql:host=localhost;port=3306;dbname=GoodFood"
 // et le constructeur du repository
+
+
+// truc pas normal avec le namespace au lieu il veut pas accepter iutnc/nrv comme ./src/classes jsp pk 
