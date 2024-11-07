cc0366eb06400a8e435313e456b25c1204a843cc
Nathan <nathan.eyer52700@gmail.com>
2024-11-07 13:29:16 +0100

    Complétion des méthodes du Repository

--- src/classes/repository/NrvRepository.php
+++ src/classes/repository/NrvRepository.php
@@ -209,8 +209,7 @@ class NrvRepository
                 ':url' => $show->url
             ]);
         }else{
-            header("../action/defaultAction");
-            exit();
+
         }
     }
 
