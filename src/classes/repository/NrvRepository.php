609669ce495aebd59dd2b7294cb5789a5d868f52
Yeahboys04 <ryankorban2004@gmail.com>
2024-11-07 08:41:35 +0100

    Ajout du squelette des méthodes du repo

--- src/classes/repository/NrvRepository.php
+++ src/classes/repository/NrvRepository.php
@@ -45,4 +45,78 @@ class NrvRepository
         }
         return self::$instance;
     }
+
+// 1. Affichage de la liste des spectacles
+    function findAllShows() : array
+    {
+        // TODO
+    }
+
+// 2. Filtrage de la liste des spectacles par date
+    function findShowsByDate(string $date) : array
+    {
+        // TODO
+    }
+
+// 3. Filtrage de la liste des spectacles par style de musique
+    function findShowsByStyle(string $style) : array
+    {
+        // TODO
+    }
+
+// 4. Filtrage de la liste des spectacles par lieu
+    function findShowsByLocation(string $location) : array
+    {
+        // TODO
+    }
+
+// 5. Affichage détaillé d’un spectacle
+    function findShowDetails(int $showId) : array
+    {
+        // TODO
+    }
+
+// 6. Affichage du détail d’une soirée
+    function findEventDetails(int $eventId) : array
+    {
+        // TODO
+    }
+
+// 7. Affichage du détail de la soirée correspondante en cliquant sur un spectacle
+    function findEventByShow(int $showId) : array
+    {
+        // TODO
+    }
+
+// 8. Accès aux spectacles du même lieu en lien avec un spectacle
+    function findShowsBySameLocation(int $showId) : array
+    {
+        // TODO
+    }
+
+// 9. Accès aux spectacles du même style en lien avec un spectacle
+    function findShowsBySameStyle(int $showId) : array
+    {
+        // TODO
+    }
+
+// 10. Accès aux spectacles à la même date en lien avec un spectacle
+    function findShowsBySameDate(int $showId) : array
+    {
+        // TODO
+    }
+
+// 11. Ajouter un spectacle dans la liste de préférences
+    function addShowToPreferences(int $showId) : bool
+    {
+        // TODO
+    }
+
+// 12. Afficher la liste de préférences
+    function findUserPreferences() : array
+    {
+        // TODO
+    }
+
+
 }
