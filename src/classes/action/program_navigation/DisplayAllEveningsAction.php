96af7af608113f65d0ef47c7ee78a634b1b4c2df
SolareFlame <zeldac332@gmail.com>
2024-11-12 17:31:53 +0100

    Show All Evenings

---
+++ src/classes/action/program_navigation/DisplayAllEveningsAction.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace iutnc\nrv\action\program_navigation;
+
+
+use Exception;
+use iutnc\nrv\action\Action;
+use iutnc\nrv\object\Show;
+use iutnc\nrv\render\ArrayRenderer;
+use iutnc\nrv\render\Renderer;
+use iutnc\nrv\repository\NrvRepository;
+
+/**
+ * Affichage de la liste des soirée(titre, date, horaire, image)
+ */
+class DisplayAllEveningsAction extends Action
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function executePost() :string
+    {
+        return "";
+    }
+
+    /**
+     * @inheritDoc
+     * @throws Exception
+     */
+    public function executeGet(): string
+    {
+        $_SESSION['previous'] = $_SERVER['REQUEST_URI'];
+        $repository = NrvRepository::getInstance();
+        $evenings = $repository->findAllEvenings();
+        return ArrayRenderer::render($evenings, Renderer::COMPACT,true);
+    }
+}
