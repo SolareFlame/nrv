725bb91265b2788322bef088c0e8bf340d77e49c
SolareFlame <zeldac332@gmail.com>
2024-11-15 00:33:44 +0100

    ➕ Added filters (by date, location and style)

--- src/classes/action/program_navigation/DisplayAllShowsAction.php
+++ src/classes/action/program_navigation/DisplayAllShowsAction.php
@@ -34,6 +34,61 @@ class DisplayAllShowsAction extends Action
         $_SESSION['previous'] = $_SERVER['REQUEST_URI'];
         $repository = NrvRepository::getInstance();
         $shows = $repository->findAllShows();
+
+        // GESTION DES STYLES
+        $styles = $repository->findAllStylesRAW();
+        $style_options = "";
+        foreach ($styles as $style) {
+            $style_options .= "<a href='index.php?actions=FILTRESTYLE&id={$style['style_id']}' class='filter-btn'>{$style['style_name']}</a>";
+        }
+
+        // GESTION DES LOCATIONS
+        $locations = $repository->findAllLocations();
+        $location_options = "";
+        foreach ($locations as $location) {
+            $location = unserialize($location);
+            $location_options .= "<a href='index.php?actions=FILTRELOC&id={$location->id}' class='filter-btn'>{$location->name}</a>";
+        }
+
+        // GESTION DES DATES
+        $dates_options = "";
+        for ($i = 1; $i <= 15; $i++) {
+            $day = str_pad($i, 2, '0', STR_PAD_LEFT);
+            $dates_options .= "<a href='index.php?actions=FILTREDATE&id=2025-07-{$day}' class='filter-btn'>{$day} juillet</a>";
+        }
+
+        $html = <<<HTML
+                <div class="d-flex align-items-center justify-content-center my-4 px-4">
+                    <div class="mx-2 title-border" style="background-color: #2ec5b6"></div>
+                    <h1 class="text-center mx-3">RECHERCHER</h1>
+                    <div class="mx-2 title-border" style="background-color: #2ec5b6"></div>
+                </div>
+                
+                <div class="d-flex justify-content-center gap-2">
+                    <p>Filtrez vos résultats par</p>
+                </div>
+                
+
+                <div class="d-flex justify-content-center gap-2">
+                <button class="filter-btn" onclick="showOptions('style')">STYLE</button>
+                <button class="filter-btn" onclick="showOptions('date')">DATE</button>
+                <button class="filter-btn" onclick="showOptions('location')">LIEU</button>
+                </div>
+                
+                <div class="d-flex justify-content-center gap-2 my-4" id="filter-options">
+                <div id="style" class="filter-options" style="display: none;">
+                    {$style_options}
+                </div>
+                <div id="date" class="filter-options" style="display: none;">
+                    {$dates_options}
+                </div>
+                <div id="location" class="filter-options" style="display: none;">
+                    {$location_options}
+                </div> 
+                </div>
+                
+                <script src="src/js/filter.js"></script>
+HTML;
         $user = AuthnProvider::getSignedInUser();
         $boutonAjouter = "";
         if ($user["role"] >= Authz::STAFF) {
@@ -41,6 +96,9 @@ class DisplayAllShowsAction extends Action
             <a href="?action=add-show" class="btn btn-primary m-5">Ajouter un Spectacle</a>
             HTML;
         }
-        return ArrayRenderer::render($shows, Renderer::COMPACT, true) . $boutonAjouter;
+
+        $html .= ArrayRenderer::render($shows, Renderer::COMPACT, true). $boutonAjouter;
+
+        return $html;
     }
 }
