c6264767c0d54bbe90429cff456bc99e9a16f7d1
SolareFlame <zeldac332@gmail.com>
2024-11-15 13:01:43 +0100

    🔧 Fixed merge edits

--- src/classes/render/ShowRenderer.php
+++ src/classes/render/ShowRenderer.php
@@ -3,7 +3,6 @@
 namespace iutnc\nrv\render;
 
 use iutnc\nrv\action\program_navigation\DisplayShowsByLocationAction;
-use iutnc\nrv\exception\RepositoryException;
 use iutnc\nrv\object\Show;
 use iutnc\nrv\repository\NrvRepository;
 use iutnc\nrv\auth\AuthnProvider;
@@ -99,8 +98,6 @@ HTML;
 
         $id = $this->show->id;
 
-
-
         $heart = !in_array($id, $_SESSION['favorites'])
             ? "<a href='?action=addShow2Fav&id={$id}' class='favorite-icon'><img src='res/icons/heart_void.png' alt='not liked'></a>"
             : "<a href='?action=delShow2fav&id={$id}' class='favorite-icon'><img src='res/icons/heart_full.png' alt='liked'></a>";
@@ -129,65 +126,9 @@ HTML;
             }
         }
 
-        try {
-            $evening = NrvRepository::getInstance()->findEveningOfShow($id);
-            $showsByLoc = NrvRepository::getInstance()->findShowsByLocation($evening->location->id);
-            foreach ($showsByLoc as $key => $show) {
-                $show = unserialize($show);
-                if ($show->id == $this->show->id) {
-                    unset($showsByLoc[$key]);
-                    break;
-                }
-            }
-            $showsByLoc = ArrayRenderer::render($showsByLoc, Renderer::COMPACT, true);
-        } catch (RepositoryException $e){
-            $showsByLoc = "";
-        }
-
-
-
-        $showsByStyle = NrvRepository::getInstance()->findShowsByStyle(NrvRepository::getInstance()->findIdStyleByStyleValue($this->show->style));
-        foreach ($showsByStyle as $key => $show) {
-            $show = unserialize($show);
-            if ($show->id == $this->show->id) {
-                unset($showsByStyle[$key]);
-                break;
-            }
-        }
-        $showsByStyle =  ArrayRenderer::render($showsByStyle, Renderer::COMPACT, true);
-
-
-
-        $showsByDay = NrvRepository::getInstance()->findShowsByDate($this->show->startDate);
-        foreach ($showsByDay as $key => $show) {
-            $show = unserialize($show);
-            if ($show->id == $this->show->id) {
-                unset($showsByDay[$key]);
-                break;
-            }
-        }
-        $showsByDay = ArrayRenderer::render($showsByDay, Renderer::COMPACT, true);
-
         $inst = NrvRepository::getInstance();
-        try {
-            $evening_parent = $inst->findEveningOfShow($this->show->id);
-            $evening_parent_loc = $evening_parent->location;
-            $icon = <<<HTML
-                            <p><i class="fas fa-star info-icon me-2"></i><a href="index.php?action=evening&id={$evening_parent->id}" class="text-decoration-none">{$evening_parent->title}</a></p>
-HTML;
-            $filtreLieu = <<<HTML
-                <a href='index.php?actions=FILTRELOC&id={$evening_parent_loc->id}' class='filter-btn'>LIEU: {$evening_parent_loc->name}</a>
-HTML;
-
-
-
-        } catch (RepositoryException $e){
-            $icon = <<<HTML
-<p><i class="fas fa-star info-icon me-2"></i><a class="text-decoration-none">Associée à aucun spectacle</a></p>
-HTML;
-            $filtreLieu = "";
-        }
-
+        $evening_parent = $inst->findEveningOfShow($this->show->id);
+        $evening_parent_loc = $evening_parent->location;
 
         $date = $this->show->startDate->format('Y-m-d');
 
@@ -223,7 +164,7 @@ HTML;
                             
                             <p><i class="fas fa-calendar-alt info-icon me-2"></i>{$this->show->startDate->format('d M Y \à H:i')}</p>
                             <p><i class="fas fa-clock info-icon me-2"></i>{$heures}h{$minutes}</p>
-                            $icon
+                            <p><i class="fas fa-star info-icon me-2"></i><a href="index.php?action=evening&id={$evening_parent->id}" class="text-decoration-none">{$evening_parent->title}</a></p>
                             <p><i class="fas fa-tags info-icon me-2"></i>{$this->show->style}</p>
                             <p><i class="fas fa-comment info-icon me-2"></i>Description</p>
             
@@ -265,30 +206,11 @@ HTML;
                     <h1 class="text-center mx-3">SPECTACLES SIMILAIRES</h1>
                     <div class="mx-2 title-border" style="background-color: #2ec5b6"></div>
                 </div>
-               
-               <div class="d-flex justify-content-center gap-2">
-                <a href='index.php?actions=FILTRESTYLE&id={$show_id}' class='filter-btn'>STYLE: {$this->show->style}</a>
-                <a href='index.php?actions=FILTREDATE&id={$date}' class='filter-btn'>DATE: {$date}</a>
-                $filtreLieu
-                </div>
-              
-                <div>
-                <br><h2>Spectacle ayant lieu le même jour</h2><br>
-                
-                    {$showsByDay}
-                
-                </div>
-                
-                <br><h2>Spectacle au même lieu</h2><br>
-                
-                    {$showsByLoc}
-             
-                
-                <div>
-                <br><h2>Spectacle du même style</h2><br>
-                
-                    {$showsByStyle}
-                
+
+                <div class="d-flex justify-content-center gap-2">
+                <a href='index.php?action=filterBySytle&id={$show_id}' class='filter-btn'>STYLE: {$this->show->style}</a>
+                <a href='index.php?action=filterByDate&id={$date}' class='filter-btn'>DATE: {$date}</a>
+                <a href='index.php?action=filterByLocation&id={$evening_parent_loc->id}' class='filter-btn'>LIEU: {$evening_parent_loc->name}</a>
                 </div>
             HTML;
 
